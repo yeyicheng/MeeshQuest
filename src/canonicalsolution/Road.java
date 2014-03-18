f144eae5e720b4deb5fe7dd4edbe6eaa90e4f177
zinglax <zinglax@github.com>
2014-03-17 23:37:46 -0400

    added test generation files and road class (which should have been added earlier)

---
+++ src/canonicalsolution/Road.java
@@ -0,0 +1,29 @@
+package canonicalsolution;
+
+import java.awt.geom.Line2D;
+
+public class Road {
+	
+	private City[] cities = new City[2];
+	private Line2D line;
+	
+	public Road(City s_city, City e_city) {
+		cities[0] = s_city;
+		cities[1] = e_city;
+		
+		line = new java.awt.geom.Line2D.Double(
+				s_city.getX(), s_city.getY(), e_city.getX(), e_city.getY());
+	}
+
+	public Line2D getLine() {
+		return line;
+	}
+
+	public void setLine(Line2D line) {
+		this.line = line;
+	}
+	 
+	// TODO add city array
+	// TODO add line2d object
+	// TODO add getters and setters
+}
---
+++ test_generation/meeshtestpart2.py
@@ -0,0 +1,71 @@
+import sys
+from xml.dom import minidom
+
+_IMPL = minidom.getDOMImplementation()
+_ROOT_ATTRS = {
+        "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
+        "xsi:noNamespaceSchemaLocation": "part2in.xsd"
+        }
+
+class Commands(object):
+    def do_city(self, doc, name, x, y):
+        me = doc.createElement("createCity")
+        me.setAttribute("name", name)
+        me.setAttribute("x", x)
+        me.setAttribute("y", y)
+        me.setAttribute("radius", "5")
+        me.setAttribute("color", "black")
+        return me
+        
+    def do_mapCity(self, doc, name):
+        me = doc.createElement("mapCity")
+        me.setAttribute("name", name)
+        return me
+    
+    def do_road(self, doc, start, end):
+        me = doc.createElement("mapRoad")
+        me.setAttribute("start", start)
+        me.setAttribute("end", end)
+        return me
+
+    def do_savemap(self, doc, name):
+        me = doc.createElement("saveMap")
+        me.setAttribute("name", name)
+        return me
+
+    def do_delete(self, doc, name):
+        me = doc.createElement("deleteCity")
+        me.setAttribute("name", name)
+        return me
+
+def main(f):
+    doc = _IMPL.createDocument(None, "commands", None)
+    id = 1
+    root = doc.documentElement
+    for (name, attr) in _ROOT_ATTRS.items():
+        root.setAttribute(name, attr)
+    cmd_attrs = f.readline().split()
+    # first line should be WIDTH HEIGHT PMORDER LEAFORDER
+    # space-separated, in that order
+    root.setAttribute("spatialWidth", cmd_attrs[0])
+    root.setAttribute("spatialHeight", cmd_attrs[1])
+    root.setAttribute("pmOrder", cmd_attrs[2])
+    root.setAttribute("g", cmd_attrs[3])
+    c = Commands()
+    for line in f:
+        args = line.split()
+        cmd = args.pop(0)
+        fname = "do_" + cmd
+        if hasattr(c, fname):
+            fn = getattr(c, fname)
+            elt = fn(doc, *args)
+        else:
+            elt = doc.createElement(cmd)
+        elt.setAttribute("id", str(id))
+        id += 1
+        root.appendChild(elt)
+    print(doc.toprettyxml())
+
+if __name__ == '__main__':
+    with open(sys.argv[1], "r") as f:
+        main(f)
